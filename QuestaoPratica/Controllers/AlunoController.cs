0e3c17857c106bc8c35ad594d1c4b8ee25df1307
Gabriel Alves Carvalho <gabriel_alvescarvalho@hotmail.com>
2023-08-21 13:18:40 -0300

    Adicionar arquivos de projeto.

---
+++ QuestaoPratica/Controllers/AlunoController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuestaoPratica.Models.Request;
+
+namespace QuestaoPratica.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController()
+        {
+            _alunoCaminhoArquivo = Path.Combine(Directory.GetCurrentDirectory(), "Data", "aluno.json");
+        }
+
+        private readonly string _alunoCaminhoArquivo;
+
+        #region Metodos
+        private List<AlunoViewModel> LerAlunosDoArquivo()
+        {
+            if (!System.IO.File.Exists(_alunoCaminhoArquivo))
+            {
+                return new List<AlunoViewModel>();
+            }
+
+            string json = System.IO.File.ReadAllText(_alunoCaminhoArquivo);
+            if (string.IsNullOrEmpty(json))
+            {
+                return new List<AlunoViewModel>();
+            }
+            return JsonConvert.DeserializeObject<List<AlunoViewModel>>(json);
+        }
+
+        private int ObterProximoCodigoDisponivel()
+        {
+            List<AlunoViewModel> alunos = LerAlunosDoArquivo();
+            if (alunos.Any())
+            {
+                return alunos.Max(p => p.Codigo) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private void EscreverAlunosNoArquivo(List<AlunoViewModel> alunos)
+        {
+            string json = JsonConvert.SerializeObject(alunos);
+            System.IO.File.WriteAllText(_alunoCaminhoArquivo, json);
+        }
+
+
+
+        #endregion
+
+        #region Operações CRUD
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<AlunoViewModel> alunos  = LerAlunosDoArquivo();
+            return Ok(alunos);
+        }
+
+        [HttpGet("{codigo}")]
+        public IActionResult Get(int codigo)
+        {
+            List<AlunoViewModel> alunos = LerAlunosDoArquivo();
+            AlunoViewModel aluno = alunos.Find(a => a.Codigo == codigo);
+            if(aluno == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]AlunoViewModel aluno)
+        {
+            if(aluno == null)
+            {
+                return BadRequest();
+            }
+
+            List<AlunoViewModel> alunos = LerAlunosDoArquivo();
+            int proximoCodigo = ObterProximoCodigoDisponivel();
+
+            AlunoViewModel novoAluno = new AlunoViewModel()
+            {
+                Codigo = proximoCodigo,
+                Nome = aluno.Nome,
+                Ra = aluno.Ra,
+                Email = aluno.Email,
+                Cpf = aluno.Cpf,
+                Ativo = aluno.Ativo,
+            };
+
+            alunos.Add(novoAluno);
+            EscreverAlunosNoArquivo(alunos);
+
+            return CreatedAtAction(nameof(Get), new { codigo = novoAluno.Codigo }, novoAluno);
+        }
+
+        [HttpPut("{codigo}")]
+        public IActionResult Put(int codigo, [FromBody] EditaAlunoViewModel aluno)
+        {
+            if (aluno == null)
+            {
+                return BadRequest();
+            }
+            List<AlunoViewModel> alunos = LerAlunosDoArquivo();
+            int index = alunos.FindIndex(p => p.Codigo == codigo);
+
+            if (index == -1)
+            {
+                return NotFound();
+            }
+
+            AlunoViewModel alunoEditado = new AlunoViewModel()
+            {
+                Codigo = codigo,
+                Nome = aluno.Nome,
+                Ra = aluno.Ra,
+                Email = aluno.Email,
+                Cpf = aluno.Cpf,
+                Ativo = aluno.Ativo,
+            };
+
+            alunos[index] = alunoEditado;
+            EscreverAlunosNoArquivo(alunos);
+            return NoContent();
+        }
+
+        [HttpDelete("{codigo}")]
+        public IActionResult Delete(int codigo)
+        {
+            List<AlunoViewModel> alunos = LerAlunosDoArquivo();
+            AlunoViewModel aluno = alunos.Find(a => a.Codigo == codigo);
+            if(aluno == null)
+                return NotFound();
+
+            alunos.Remove(aluno);
+            EscreverAlunosNoArquivo(alunos);
+            return NoContent();
+        }
+
+            #endregion
+        
+    }
+}
---
+++ QuestaoPratica/Models/Request/AlunoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace QuestaoPratica.Models.Request
+{
+    public class AlunoViewModel
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Ra { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
